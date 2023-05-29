b5e55084dada73bba6a14c532e9cafb2e085a36a
ZainGillani <gillanizain90@gmail.com>
2023-05-29 12:46:12 +0500

    .

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -21,98 +21,100 @@ class _ProfileScreenState extends State<ProfileScreen> {
       appBar: AppBar(
         title: const Text('Profile'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: CircleAvatar(
-                radius: 80,
-                // backgroundImage: AssetImage('assets/images/profile_image.jpg'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Center(
+                child: CircleAvatar(
+                  radius: 80,
+                  // backgroundImage: AssetImage('assets/images/profile_image.jpg'),
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            Consumer<UserProvider>(
-              builder: (context, value, child) {
-                return Text(
-                  value.user!.name,
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                );
-              },
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Software Developer',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[600],
+              const SizedBox(height: 16),
+              Consumer<UserProvider>(
+                builder: (context, value, child) {
+                  return Text(
+                    value.user!.name,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                },
               ),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'About Me',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+              const SizedBox(height: 8),
+              Text(
+                'Software Developer',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[600],
+              const SizedBox(height: 16),
+              const Text(
+                'About Me',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Contact',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+              const SizedBox(height: 8),
+              Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Consumer<UserProvider>(
-              builder: (context, value, child) {
-                return ListTile(
-                  leading: Icon(Icons.email),
-                  title: Text(value.user!.email),
-                );
-              },
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () async {
-                // Handle logout button press
-                final auth = AuthMethods();
-                await auth.signOut();
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AuthenticationScreen(),
-                    ));
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red,
-                onPrimary: Colors.white,
+              const SizedBox(height: 16),
+              const Text(
+                'Contact',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              child: const Text('Logout'),
-            ),
-            Switch(
-              value: isDarkModeEnabled,
-              onChanged: (value) {
-                themeProvider.toggleTheme(value);
-              },
-            ),
-            isDarkModeEnabled
-                ? Text("Enable Light Mode")
-                : Text("Enable Dark Mode")
-          ],
+              const SizedBox(height: 8),
+              Consumer<UserProvider>(
+                builder: (context, value, child) {
+                  return ListTile(
+                    leading: Icon(Icons.email),
+                    title: Text(value.user!.email),
+                  );
+                },
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () async {
+                  // Handle logout button press
+                  final auth = AuthMethods();
+                  await auth.signOut();
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AuthenticationScreen(),
+                      ));
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  onPrimary: Colors.white,
+                ),
+                child: const Text('Logout'),
+              ),
+              Switch(
+                value: isDarkModeEnabled,
+                onChanged: (value) {
+                  themeProvider.toggleTheme(value);
+                },
+              ),
+              isDarkModeEnabled
+                  ? Text("Enable Light Mode")
+                  : Text("Enable Dark Mode")
+            ],
+          ),
         ),
       ),
     );
